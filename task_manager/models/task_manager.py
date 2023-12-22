31bb7a17dfa449fdcb92bf2977ea7c44a4a2b0a1
Tamjeed Anees <tamjeedanees1995@gmail.com>
2023-12-22 13:05:59 +0500

    Add new functionality for count remaining days for the task according to the deadline.

--- task_manager/models/task_manager.py
+++ task_manager/models/task_manager.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import UserError
+from datetime import datetime
 
 class Task(models.Model):
     _name = 'task.manager'
@@ -16,11 +17,25 @@ class Task(models.Model):
         ('progress', 'In Progress'), 
         ('completed', 'Completed')
     ], default='progress', string="State")
+    days_remaining = fields.Integer(string="Days Remaining", compute='_compute_days_remaining', store=True)
+
+    @api.onchange('deadline')
+    def onchange_deadline(self):
+        if self.deadline and fields.Date.from_string(self.deadline) < datetime.now().date():
+            raise UserError("Deadline date cannot be less than the current date.")
     
-    
+    @api.depends('deadline')
+    def _compute_days_remaining(self):
+        for rec in self:
+            if rec.deadline:
+                deadline_date = fields.Date.from_string(rec.deadline)
+                today_date = datetime.now().date()
+                remaining_days = (deadline_date - today_date).days
+                rec.days_remaining = remaining_days if remaining_days > 0 else 0
+                
     def action_completed(self):
         for rec in self:
             rec.write({
                 'state': 'completed',
-                'completed': True
+                'task_completed': True
             })
